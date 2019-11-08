48b74fef54cf420061424c21cdd0a6a60def30b8
victortiads <baappaap123@hotmail.com>
2019-11-08 17:27:36 -0300

    Update

--- app/src/main/java/com/example/projetoandroidbsico/Adapter/Adapter.kt
+++ app/src/main/java/com/example/projetoandroidbsico/Adapter/Adapter.kt
@@ -6,11 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.projetoandroidbsico.Books
 import com.example.projetoandroidbsico.ListItem
 import com.example.projetoandroidbsico.R
 import kotlinx.android.synthetic.main.listview.view.*
 
-class Adapter (private  val context: Context, var list: ListItem): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class Adapter (private  val context: Context, var list: ArrayList<Books>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
 
@@ -21,21 +22,36 @@ class Adapter (private  val context: Context, var list: ListItem): RecyclerView.
     }
 
     override fun getItemCount(): Int {
-        return list.books.size
+        return list.size
 
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
-        var itemList = list.books[position]
+        var itemList = list[position]
         var title = holder.itemView.textView_Title
         var subtitle = holder.itemView.textView_Subtitle
+        var btnDelete = holder.itemView.ImageButton_Delete
+
 
         title.text = itemList.title
         subtitle.text = itemList.subtitle
 
+        btnDelete.setOnClickListener {
+            list.remove(itemList)
+            this.notifyItemRemoved(holder.adapterPosition)
+        }
+
+
     }
 
+    fun addItem(book:ArrayList<Books>){
+        list = book
+        this.notifyItemRangeInserted(itemCount,itemCount)
+    }
+
+
 }
+
 
 class ViewHolder (val itemView: View) : RecyclerView.ViewHolder(itemView)
--- app/src/main/java/com/example/projetoandroidbsico/MainActivityList.kt
+++ app/src/main/java/com/example/projetoandroidbsico/MainActivityList.kt
@@ -1,14 +1,26 @@
 package com.example.projetoandroidbsico
 
+import android.content.DialogInterface
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.projetoandroidbsico.Adapter.Adapter
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_main_list.*
+import kotlinx.android.synthetic.main.listview.*
+import org.jetbrains.anko.*
 
 class MainActivityList : AppCompatActivity()
 {
-    var mListBooks = ListItem(ArrayList())
+    var mListBooks = ArrayList<Books>()
+    lateinit var mtitle:EditText
+    lateinit var subtitle:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +35,48 @@ class MainActivityList : AppCompatActivity()
 
 
         button_fab.setOnClickListener{
-            
+
+            //adapter.addItem(Books("Meu nome","Zé"))
+
+            alert {
+                title = "Inserir Livro"
+
+
+                customView {
+
+                    linearLayout {
+                        this.orientation = LinearLayout.VERTICAL
+                          mtitle = editText {
+                            hint = "Título do Livro"
+
+                        }.lparams(width = matchParent,height = wrapContent){
+                            bottomMargin = dip(10)
+                        }
+                         subtitle = editText {
+                            hint = "Subtítulo do Livro"
+
+                        }.lparams(width = matchParent,height = wrapContent){
+                            bottomMargin = dip(10)
+                        }
+
+
+
+
+
+                        padding = dip(16)
+                    }
+
+                }
+                positiveButton("Inserir") { mListBooks.add(Books(mtitle.text.toString(),subtitle.text.toString()))
+                adapter.addItem(mListBooks)}
+            }.show()
+
         }
+
+
+
+
+
 
 
 
